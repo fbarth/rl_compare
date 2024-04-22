f242d2a954ced51144d6f766cd7c3fb80a6c8ad0
Rafael Malcervelli <r.malcervelli@gmail.com>
2024-04-22 15:06:48 -0300

    Bipedal walker PPO (env=1), Rafael Seicali Malcervelli

--- cart_pole_dqn.py
+++ cart_pole_dqn.py
@@ -5,41 +5,41 @@ from stable_baselines3.common.logger import configure
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_vec_env
 
-# tmp_path = "./results/bipedalwalker_ppo/"
-# new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+tmp_path = "./results/bipedalwalker_ppo/"
+new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
 
-# env = gym.make("BipedalWalker-v3")
-# vec_env = make_vec_env("BipedalWalker-v3", n_envs=1)
+env = gym.make("BipedalWalker-v3")
+vec_env = make_vec_env("BipedalWalker-v3", n_envs=1)
 
-# # Documentação completa em https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
-# model = PPO(
-#     policy="MlpPolicy",
-#     env=vec_env,
-#     learning_rate=3e-4,
-#     n_steps=2048,
-#     batch_size=64,
-#     n_epochs=10,
-#     gamma=0.99,
-#     gae_lambda=0.95,
-#     clip_range=0.2,
-#     clip_range_vf=None,
-#     ent_coef=0.0,
-#     vf_coef=0.5,
-#     max_grad_norm=0.5,
-#     use_sde=False,
-#     sde_sample_freq=-1,
-#     target_kl=None,
-#     tensorboard_log=None
-# )
+# Documentação completa em https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
+model = PPO(
+    policy="MlpPolicy",
+    env=vec_env,
+    learning_rate=3e-4,
+    n_steps=2048,
+    batch_size=64,
+    n_epochs=10,
+    gamma=0.99,
+    gae_lambda=0.95,
+    clip_range=0.2,
+    clip_range_vf=None,
+    ent_coef=0.0,
+    vf_coef=0.5,
+    max_grad_norm=0.5,
+    use_sde=False,
+    sde_sample_freq=-1,
+    target_kl=None,
+    tensorboard_log=None
+)
 
-# model.set_logger(new_logger)
-# model.learn(total_timesteps=1_000_000)
-# model.save("models/bipedalwalker_ppo")
+model.set_logger(new_logger)
+model.learn(total_timesteps=1_000_000)
+model.save("models/bipedalwalker_ppo")
 
-# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-# print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
 
-# del model
+del model
 model = PPO.load("models/bipedalwalker_ppo")
 
 print('modelo treinado')
