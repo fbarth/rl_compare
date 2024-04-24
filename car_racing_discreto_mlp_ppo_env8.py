b58e4c9a0e613e1e370b224936178c666a3c4e1e
renatex333 <renato9302@hotmail.com>
2024-04-23 22:48:09 -0300

    Tried my best, did not succeed.

--- car_racing_discreto_mlp_ppo_env8.py
+++ car_racing_discreto_mlp_ppo_env8.py
@@ -4,6 +4,7 @@ Module to train a PPO model with a MLP policy on the CarRacing-v1 environment wi
 
 import sys
 import gymnasium as gym
+from gymnasium.wrappers import ResizeObservation, GrayScaleObservation
 from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -14,16 +15,15 @@ RESULTS_PATH = f"./results/{BASE_PATH}/"
 MODEL_PATH = f"./models/{BASE_PATH}"
 ENV_NAME = "CarRacing-v2"
 
+def main():
+    train_model = input("Train model? (Y/n): ")
+    if train_model.lower() != "n":
+        train()
+    sys.exit(test())
+
 def train():
     new_logger = configure(RESULTS_PATH, ["stdout", "csv", "tensorboard"])
 
-    env = gym.make(
-        ENV_NAME,
-        render_mode="rgb_array",
-        lap_complete_percent=0.95,
-        domain_randomize=False,
-        continuous=False
-    )
     vec_env = make_vec_env(
         ENV_NAME,
         n_envs=8,
@@ -32,39 +32,32 @@ def train():
             "lap_complete_percent": 0.95,
             "domain_randomize": False,
             "continuous": False
-        }
+        },
+        wrapper_class=my_wrapper,
+        wrapper_kwargs={
+            "shape": (64, 64),
+            "keep_dim": True
+        },
     )
 
     # Stable-baseline PPO usage docs:
     # https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
 
-    # model = PPO(
-    #     policy="MlpPolicy",
-    #     env=vec_env,
-    #     learning_rate=1e-4,
-    #     n_steps=512,
-    #     batch_size=128,
-    #     n_epochs=10,
-    #     gamma=0.99,
-    #     gae_lambda=0.95,
-    #     clip_range=0.2,
-    #     vf_coef=0.5,
-    #     ent_coef=0.0,
-    #     max_grad_norm=0.5,
-    #     tensorboard_log=None,
-    #     verbose=1
-    # )
-
-    model = PPO.load(MODEL_PATH)
-    model.set_env(vec_env)
+    model = PPO(
+        policy="MlpPolicy",
+        env=vec_env,
+        batch_size=1024,
+        n_steps=512,
+        n_epochs=10,
+        verbose=1,
+    )
 
     model.set_logger(new_logger)
-    model.learn(total_timesteps=1_000_000)
+    model.learn(total_timesteps=500_000)
     model.save(MODEL_PATH)
 
     mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
     print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
-    env.close()
     return 0
 
 def test():
@@ -79,22 +72,22 @@ def test():
         continuous=False
     )
 
-    (obs,_) = env.reset()
+    wrapped_env = my_wrapper(env, shape=(64, 64), keep_dim=True)
+
+    (obs,_) = wrapped_env.reset()
     for i in range(1000):
         action, _state = model.predict(obs, deterministic=True)
-        # print(i, action)
-        obs, reward, done, truncated, info = env.step(action)
-        env.render()
+        obs, reward, done, truncated, info = wrapped_env.step(action)
+        wrapped_env.render()
         if done:
-            obs = env.reset()
-    env.close()
+            obs = wrapped_env.reset()
+    wrapped_env.close()
     return 0
 
-def main():
-    train_model = input("Train model? (Y/n): ")
-    if train_model.lower() != "n":
-        train()
-    sys.exit(test())
+def my_wrapper(env, shape, keep_dim):
+    resized_env = ResizeObservation(env, shape=shape)
+    gray_env = GrayScaleObservation(resized_env, keep_dim=keep_dim)
+    return gray_env
 
 if __name__ == "__main__":
     main()
