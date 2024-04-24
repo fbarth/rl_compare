ff92037a4cea3d3ea62b51e6a5bdd2f4757d3298
Fabricio J Barth <fabricio.barth@gmail.com>
2024-04-24 13:15:38 -0300

    script para analise de curva de aprendizado e execucao de modelos

--- run_models.sh
+++ run_models.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 python run_model.py CartPole-v1 models/cartpole_dqn DQN
+python run_model.py CartPole-v1 models/cartpole_ppo_env_1 PPO
+python run_model.py BipedalWalker-v3 models/bipedal_walker_a2c A2C
